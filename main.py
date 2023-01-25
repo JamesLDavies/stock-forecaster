d419f3523e2e6ffc3825ce5544729d6ba84a9d97
JamesLDavies <34004105+JamesLDavies@users.noreply.github.com>
2023-01-25 14:41:17 +0000

    Trained random forest and knn models

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ import ast
 
 import configparser
 
+import numpy as np
+from finta import TA
+import matplotlib.pyplot as plt
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn import metrics
+
 pd.options.plotting.backend = "plotly"
 
 config = configparser.ConfigParser()
@@ -95,15 +109,86 @@ def produce_prediction(df: pd.DataFrame, window: int=15) -> pd.DataFrame:
     return df
 
 
+def train_random_forest(X_train: pd.DataFrame,
+                        y_train: pd.Series,
+                        X_test: pd.DataFrame,
+                        y_test: pd.Series,
+                        print_pred: bool=False
+                        ) -> RandomForestClassifier:
+    """
+
+    Args:
+        X_train:
+        y_train:
+        X_test:
+        y_test:
+        print_pred:
+
+    Returns:
+
+    """
+    rf = RandomForestClassifier()
+    params_rf = {'n_estimators': [110, 130, 140, 150, 160, 180, 200]}
+    rf_gs = GridSearchCV(rf, params_rf, cv=5)
+    rf_gs.fit(X_train, y_train)
+    rf_best = rf_gs.best_estimator_
+
+    if print_pred:
+        pred = rf_best.predict(X_test)
+        print(classification_report(y_test, pred))
+        print(confusion_matrix(y_test, pred))
+
+    return rf_best
+
+
+def train_KNN(X_train: pd.DataFrame,
+              y_train: pd.Series,
+              X_test: pd.DataFrame,
+              y_test: pd.Series,
+              print_pred: bool=False
+              ) -> KNeighborsClassifier:
+    knn = KNeighborsClassifier()
+
+    params_knn = {'n_neighbors': np.arange(1, 25)}
+    knn_gs = GridSearchCV(knn, params_knn, cv=5)
+    knn_gs.fit(X_train, y_train)
+    knn_best = knn_gs.best_estimator_
+
+    if print_pred:
+        print(knn_gs.best_params_)
+        prediction = knn_best.predict(X_test)
+        print(classification_report(y_test, prediction))
+        print(confusion_matrix(y_test, prediction))
+
+    return knn_best
+
+
+# Feature Engineering
 df = exponential_smooth(df, 0.7)
 fig = df['close'].plot()
-fig.show()
+# fig.show()
 
 df = get_indicator_data(df, indicators)
 print(df.columns)
 
 df = produce_prediction(df, 15)
 
-df= df.dropna()
+df = df.dropna()
 
 print(df.tail())
+
+# Create Ensemble model
+y = df['pred']
+features = [x for x in df.columns if x not in ['pred']]
+X = df[features]
+
+(X_train,
+ X_test,
+ y_train,
+ y_test) = train_test_split(X, y, train_size=(7 * len(X) // 10), shuffle=False)
+
+rf_model = train_random_forest(X_train, y_train, X_test, y_test, True)
+
+knn_model = train_KNN(X_train, y_train, X_test, y_test, True)
+
+
